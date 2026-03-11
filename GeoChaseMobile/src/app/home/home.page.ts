d5cf8252cdeefe423d4a5e5cff1290cb8ec9b80e
Kerem Acar <kemracr@gmail.com>
2026-03-11 07:27:16 +0300

    Add user profile fetching and display with JWT authorization

--- GeoChaseMobile/src/app/home/home.page.ts
+++ GeoChaseMobile/src/app/home/home.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+  IonText
+} from '@ionic/angular/standalone';
+import { Auth } from '../services/auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent,
+    IonText, CommonModule
+  ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+
+  playerProfile: any = null;
+
+  constructor(private authService: Auth) {}
+
+  ngOnInit() {
+    this.loadProfile();
+  }
+
+  loadProfile(){
+    this.authService.getProfile().subscribe({
+      next: (data) => {
+        this.playerProfile = data;
+        console.log("Profile has come successfully: "+data);
+      },
+      error: (err) =>{
+        console.error("There is a problem while gethering profile: "+ err)
+      }
+    })
+  }
 }
--- GeoChaseMobile/src/app/services/auth.ts
+++ GeoChaseMobile/src/app/services/auth.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -24,6 +24,14 @@ export class Auth {
 
               })
             )
+  }
+
+  getProfile(): Observable<any> {
+    const token = localStorage.getItem('jwt_token');
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+    return this.http.get<any>(`${this.apiUrl}/profile`, {headers});
   }
   
 }
