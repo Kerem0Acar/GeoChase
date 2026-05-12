164767686199f0c466529dcbe39c2ffbc5a7d989
Kerem Acar <kemracr@gmail.com>
2026-05-12 22:07:51 +0300

    ZombieThreat class updated and make zombies smart

--- backend/src/main/java/com/GeoChase/backend/dto/ZombieThreat.java
+++ backend/src/main/java/com/GeoChase/backend/dto/ZombieThreat.java
@@ -2,9 +2,18 @@ package com.GeoChase.backend.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ZombieThreat {
     private String type;
     private double lat;
     private double lng;
+    private List<RoutePoint> route;
+
+    @Data
+    public static class RoutePoint {
+        private double lat;
+        private double lng;
+    }
 }
